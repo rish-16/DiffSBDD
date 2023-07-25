33ca4d9f23ec917925e1657d07bb258ff78fbeb8
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-07-25 11:47:08 +0100

    Add 3NY8 PDB

---
+++ run_gpcr.sh
@@ -0,0 +1,24 @@
+python generate_ligands.py checkpoints/ca_inpaint.ckpt \
+        --pdbfile gpcr_tests/3NY8_5089/11186_dyn_123_holo.pdb \
+        --outdir result_3NY8_5089/ \
+        ----ref_ligand L:JRZ
+
+# python generate_ligands.py checkpoints/ca_inpaint.ckpt \
+#         --pdbfile gpcr_tests/3NYA_5091/11198_dyn_124_holo.pdb \
+#         --outdir result_3NYA_5091/ \
+#         ----ref_ligand <chain>:<resi>
+
+# python generate_ligands.py checkpoints/ca_inpaint.ckpt \
+#         --pdbfile gpcr_tests/4LDO_5080/11159_dyn_117_holo.pdb \
+#         --outdir result_4LDO_5080/ \
+#         ----ref_ligand <chain>:<resi>
+
+# python generate_ligands.py checkpoints/ca_inpaint.ckpt \
+#         --pdbfile gpcr_tests/5D6L_5216/18127_dyn_963_holo.pdb \
+#         --outdir result_5D6L_5216/ \
+#         ----ref_ligand <chain>:<resi>
+
+# python generate_ligands.py checkpoints/ca_inpaint.ckpt \
+#         --pdbfile gpcr_tests/6PS0_5216/tmp_dyn_0_685_holo.pdb \
+#         --outdir result_6PS0_5216/ \
+#         ----ref_ligand <chain>:<resi>
