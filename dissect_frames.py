3b2eeb2e5ac856f2f61292cfdc089d54edfb0c0f
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-07-29 16:53:43 +0800

    Updates

--- dissect_frames.py
+++ dissect_frames.py
@@ -7,11 +7,12 @@ MD_HD5_PATH = "/data/rishabh/MD/h5_files/MD.hdf5"
 md_obj = h5py.File(MD_HD5_PATH)
 
 print (type(md_obj))
-print (md_obj.keys())
+print (len(md_obj.keys()))
 for i, (key, items) in enumerate(md_obj.items()):
-    print (key)
-    print (items)
+    print (key, items)
     print (type(items), type(key))
 
-    if i == 5:
+    print (dict(items))
+
+    if i == 3:
         break
