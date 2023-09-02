7901a6ba758058816b09c164c3dc7f91b060d7cb
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-03 01:56:42 +0800

    Updates

--- dypo_diffsbdd.sh
+++ dypo_diffsbdd.sh
@@ -1,10 +1,10 @@
 python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_0.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
 python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_1.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
-python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_10.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
 python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_2.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
 python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_3.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
 python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_4.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
 python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_6.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
 python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_8.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
+python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_10.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
 python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_94.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
 python generate_ligands.py ckpt/crossdocked_fullatom_cond.ckpt --pdbfile misato/1A4G_frames/1A4G_complex_frame_95.pdb --outdir misato/1A4G_results/ --ref_ligand X:1562
