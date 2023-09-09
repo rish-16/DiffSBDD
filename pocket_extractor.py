28c90aff5e5d3b677ff45b575cec7bf0a663c3a5
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-09 16:45:56 +0800

    Updates

---
+++ pocket_extractor.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pprint import pprint
+import MDAnalysis as mda
+import h5py
+import os, subprocess
+
+mdh5_file = '/data/rishabh/MD/h5_files/MD.hdf5'
+md_H5File = h5py.File(mdh5_file)
+
+pdb_codes = list(dict(md_H5File).keys())
+print ("number of PDBs:", len(pdb_codes))
+
+print (md_H5File_tiny['1A4G'].keys())
