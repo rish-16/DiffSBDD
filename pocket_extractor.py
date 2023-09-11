bfa5dc80ffdfbe4844ca8f6272f81bc847dcf5e0
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-11 10:48:09 +0800

    Updates

--- pocket_extractor.py
+++ pocket_extractor.py
@@ -5,10 +5,15 @@ import MDAnalysis as mda
 import h5py
 import os, subprocess
 
-mdh5_file = '/data/rishabh/MD/h5_files/MD.hdf5'
+mdh5_file = 'misato/all_atoms_11GS.hdf5'
 md_H5File = h5py.File(mdh5_file)
 
 pdb_codes = list(dict(md_H5File).keys())
 print ("number of PDBs:", len(pdb_codes))
 
-print (md_H5File['1A4G'].keys())
+print (md_H5File['11GS'].keys())
+print (md_H5File.get('11GS/atoms_coordinates_ref')[:])
+print (md_H5File.get('11GS/atoms_element')[:200])
+print (md_H5File.get('11GS/atoms_residue')[:200])
+print (md_H5File.get('11GS/atoms_number')[:200])
+
