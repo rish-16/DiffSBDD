4983973c81794418d2f3f7b637134958512f795f
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-11 10:52:32 +0800

    Updates

--- pocket_extractor.py
+++ pocket_extractor.py
@@ -12,7 +12,14 @@ pdb_codes = list(dict(md_H5File).keys())
 print ("number of PDBs:", len(pdb_codes))
 
 pprint (md_H5File['1A4G'].keys())
-print (md_H5File.get('1A4G/atoms_coordinates_ref')[:])
-print (md_H5File.get('1A4G/atoms_element')[:20])
-print (md_H5File.get('1A4G/atoms_residue')[:20])
-print (md_H5File.get('1A4G/atoms_number')[:20])
+# print (md_H5File.get('1A4G/atoms_coordinates_ref')[:])
+# print (md_H5File.get('1A4G/atoms_element')[:20])
+# print (md_H5File.get('1A4G/atoms_residue')[:20])
+# print (md_H5File.get('1A4G/atoms_number')[:20])
+
+
+pprint (md_H5File['1CNX'].keys())
+# print (md_H5File.get('1CNX/atoms_coordinates_ref')[:])
+# print (md_H5File.get('1CNX/atoms_element')[:20])
+# print (md_H5File.get('1CNX/atoms_residue')[:20])
+# print (md_H5File.get('1CNX/atoms_number')[:20])
