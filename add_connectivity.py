154de003b760a2a171b5216c0208c6cf40d1a2d7
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-17 11:38:47 +0800

    Add bond corrector script

--- add_connectivity.py
+++ add_connectivity.py
@@ -7,12 +7,12 @@ for pdb in os.listdir(PATH):
     pdb_path = PATH + pdb
     for frame_file in os.listdir(pdb_path):
         pdb_file_name = frame_file[:-4]
-        frame_uni = mda.Universe(pdb_path + frame_file, format="PDB")
+        frame_uni = mda.Universe(pdb_path + "/" + frame_file, format="PDB")
         guessed_bonds = mda.topology.guessers.guess_bonds(frame_uni.atoms, frame_uni.atoms.positions)
         frame_uni.add_TopologyAttr('bonds', guessed_bonds)
         n_atoms = frame_uni.atoms
 
-        print (pdb_path + pdb_file_name + "_bonds.pdb")
+        print (pdb_path + "/" + pdb_file_name + "_bonds.pdb")
 
-        # with PDBWriter(f"{pdb_path + frame_file}_bonds.pdb", n_atoms=n_atoms, bonds='conect') as W:
+        # with PDBWriter(f"{pdb_path + "/" + frame_file}_bonds.pdb", n_atoms=n_atoms, bonds='conect') as W:
         #     W.write(frame_uni)
