bd3920511e71e19c44ae682a6baa9b4301f06c85
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-09-17 12:01:56 +0800

    Updates

--- fix_file_ext.py
+++ fix_file_ext.py
@@ -6,10 +6,10 @@ for file in os.listdir(PATH):
     if "bio1" in file:
         fp = PATH + file
         new_fp = file[:-5]
-        cmd = f"mv {file} {new_fp}_1.pdb"
+        cmd = f"mv {fp} {PATH + new_fp}_1.pdb"
         subprocess.call(cmd, shell=True)
     elif "bio2" in file:
         fp = PATH + file
         new_fp = file[:-5]
-        cmd = f"mv {file} {new_fp}_2.pdb"
+        cmd = f"mv {fp} {PATH + new_fp}_2.pdb"
         subprocess.call(cmd, shell=True)
